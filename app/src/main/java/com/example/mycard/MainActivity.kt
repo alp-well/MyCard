e8d71a19f144dc6fbf0880a1f477fbdfd497cc06
alperen acikgoz <alperen.acikgoz@well.co>
2024-02-12 11:31:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mycard/MainActivity.kt
@@ -0,0 +1,191 @@
+package com.example.mycard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mycard.ui.theme.MyCardTheme
+import com.example.mycard.ui.theme.body2
+import com.example.mycard.ui.theme.header5
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    Column {
+                        Spacer(modifier = Modifier.size(20.dp))
+                        LayoutSolution()
+                        Spacer(modifier = Modifier.size(20.dp))
+                        ConstraintLayoutSolution()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LayoutSolution() {
+    Card(
+        modifier =
+        Modifier
+            .padding(start = 20.dp, end = 20.dp, top = 4.dp, bottom = 12.dp)
+            .fillMaxWidth(),
+    ) {
+        Layout(
+            content = {
+                Column(
+                    modifier =
+                        Modifier.padding(
+                            start = 16.dp,
+                            end = 12.dp,
+                            bottom = 16.dp,
+                            top = 16.dp,
+                        ),
+                ) {
+                    HeaderText()
+                    BodyText()
+                }
+                Box(
+                    modifier = Modifier.width(100.dp),
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.image),
+                        contentDescription = null,
+                        contentScale = ContentScale.FillWidth,
+                        modifier =
+                        Modifier
+                            .fillMaxSize()
+                            .align(Alignment.Center),
+                    )
+                }
+            },
+        ) { measurables, constraints ->
+            val columnConstraints =
+                constraints.copy(
+                    maxWidth = constraints.maxWidth - 100.dp.roundToPx(),
+                )
+            val column = measurables[0].measure(columnConstraints)
+            val boxConstraints =
+                columnConstraints.copy(
+                    maxWidth = 100.dp.roundToPx(),
+                    maxHeight = column.height,
+                )
+            val box = measurables[1].measure(boxConstraints)
+            layout(constraints.maxWidth, column.height) {
+                column.placeRelative(x = 0, y = 0)
+                val imageX = constraints.maxWidth - box.width
+                box.placeRelative(x = imageX, y = 0)
+            }
+        }
+    }
+}
+
+@Composable
+fun ConstraintLayoutSolution() {
+    Card(
+        modifier =
+        Modifier
+            .padding(start = 20.dp, end = 20.dp, top = 4.dp, bottom = 12.dp)
+            .fillMaxWidth(),
+    ) {
+        ConstraintLayout(modifier = Modifier.fillMaxWidth()) {
+            val (column, image) = createRefs()
+
+            Column(
+                modifier = Modifier
+                    .constrainAs(column) {
+                        start.linkTo(parent.start)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                        end.linkTo(parent.end)
+                    }
+                    .padding(start = 16.dp, end = 112.dp, top = 16.dp, bottom = 16.dp)
+            ) {
+                HeaderText()
+                BodyText()
+            }
+
+            Image(
+                painter = painterResource(R.drawable.image),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .constrainAs(image) {
+                        width = Dimension.value(100.dp)
+                        height = Dimension.value(160.dp)
+                        top.linkTo(parent.top, margin = (-30).dp)
+                        bottom.linkTo(parent.bottom, margin = (-30).dp)
+                        end.linkTo(parent.end)
+                    }
+
+            )
+    }
+}}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun LayoutPrev() {
+    LayoutSolution()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ConstrainLayoutPrev() {
+    ConstraintLayoutSolution()
+}
+
+@Composable
+fun HeaderText() {
+    Text(
+        text = "Lorem ipsum",
+        style = MaterialTheme.typography.header5.copy(
+            color = Color.Black,
+        ),
+    )
+}
+
+@Composable
+fun BodyText() {
+    Text(
+        modifier = Modifier.padding(top = 4.dp),
+        text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        style =
+        MaterialTheme.typography.body2.copy(
+            color = Color.Black,
+        ),
+    )
+}
+
+
---
+++ app/src/main/java/com/example/mycard/ui/theme/Type.kt
@@ -0,0 +1,56 @@
+package com.example.mycard.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.mycard.R
+
+private val ubuntu = FontFamily(
+    Font(R.font.ubuntu),
+)
+// Set of Material typography styles to start with
+val Typography =
+    Typography(
+        bodyLarge =
+            TextStyle(
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp,
+                lineHeight = 24.sp,
+                letterSpacing = 0.5.sp,
+            ),
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+     */
+
+    )
+
+val Typography.body2: TextStyle
+    get() = TextStyle(
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp,
+        //fontFamily = ubuntu
+    )
+
+val Typography.header5: TextStyle
+    get() = TextStyle(
+        fontWeight = FontWeight.W600,
+        fontSize = 16.sp,
+        //fontFamily = ubuntu
+    )
